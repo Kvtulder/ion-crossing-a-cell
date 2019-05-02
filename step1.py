691b62f19b46025438a4090568d56dca2a95a709
Madelon <madelon@pollekeg.com>
2019-05-02 13:50:53 +0200

    stap 1 met random kracht probeersel

--- step1.py
+++ step1.py
@@ -3,18 +3,34 @@ import numpy as np
 
 xstart = 0
 vstart = 0
-tend = 500
+tend = 50
 dt = 0.01
 x = xstart
 v = vstart
-m = 27e-12
+
+m = 1
+mu = 1
+#1.38064852(79)×10−23
+kb = 1
+T = 300
+D = mu * kb * T
+
+gam = .19
+
 
 L_v = []
 L_x = []
 L_t = []
 
 def F(x):
-    return x/2 - (x**3)/2
+    # https://en.wikipedia.org/wiki/Langevin_dynamics; R(t) is deze delta
+    # wat is de standaarddev hier?
+    # mu, sigma
+    delta = np.random.normal(0, .5)
+    fr = 2 * D * gam**2 * delta # delta ding
+
+    F = x/2 - (x**3)/2 + fr
+    return F
 
 for t in np.arange(0, tend, dt):
     Fo = F(x)
@@ -24,6 +40,7 @@ for t in np.arange(0, tend, dt):
     L_x.append(x)
     L_v.append(v)
     L_t.append(t)
+    #print(x)
 
 plt.plot(L_t,L_x)
 #plt.plot(L_v, L_t)
